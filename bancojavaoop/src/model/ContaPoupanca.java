a707070a7785b598909babadf07cb43bcc1d9315
KevinAp-5 <keven.clash3@gmail.com>
2025-04-08 20:09:32 -0300

    novos exercicios

---
+++ bancojavaoop/src/model/ContaPoupanca.java
@@ -0,0 +1,19 @@
+package model;
+
+public class ContaPoupanca {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
